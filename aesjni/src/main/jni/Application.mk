4541324399a4499674ba0883d5ed1d5d78c5a0cb
Qingtian <vfishv@gmail.com>
2019-09-20 18:33:40 +0800

    remove armeabi

--- aesjni/src/main/jni/Application.mk
+++ aesjni/src/main/jni/Application.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-APP_ABI :=  armeabi  armeabi-v7a arm64-v8a x86_64
+APP_ABI :=  armeabi-v7a arm64-v8a x86_64
 
 
 NDK_TOOLCHAIN_VERSION := clang-ollvm4.0
